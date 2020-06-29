3dc5660fe5606a953d7b89ab0e0be2bad8f6c0c7
megan <meggsnbacon777@gmail.com>
2020-06-29 08:41:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,7 +9,7 @@ end
 introduction_with_language("Megan","Javascript")
 
 def introduction_with_language_optional(name,language= "Ruby")
-  puts" Hi, my name is #{name} and I am learning #{language}."
+  puts"Hi, my name is #{name} and I am learning to program in #{language}."
 end
 introduction_with_language_optional("Megan")
   
